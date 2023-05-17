667ec366104972b8cb57c94996f323aacf5e8b26
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-05-17 19:05:09 +0900

    Add :: control page

---
+++ control.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="css/manage.css">
+<html lang="ko">
+<head>
+    <title>원격 제어</title>
+</head>
+<body style="margin:0px;">
+<header>
+<?php
+    include 'header.php';
+    ?>
+</header>
+<div>
+
+</div>
+<footer>
+    <?php
+    include 'footer.php';
+    ?>
+</footer>
+</body>
+</html>
