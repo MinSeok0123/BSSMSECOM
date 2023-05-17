939bf2d024cc0594d4b806a6a356704e6e7ddaa6
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-05-17 17:40:18 +0900

    Fix :: 도넛차트 스타일링

--- chart.php
+++ chart.php
@@ -15,26 +15,26 @@ $row = mysqli_fetch_assoc($result);
 <body>
 <div style="width:85%; justify-content: space-between; align-items: center; display:flex;">
 <div style="position:relative;width:280px; height:170px; box-shadow: 0px 11px 30px rgba(0, 0, 0, 0.25); border-radius:10px;">
-<div style="width:120px; margin:8px;">
+<div style="width:120px; margin-left:25px; margin-top:25px;">
     <canvas id="temperatureChart"></canvas>
 </div>
-<div style="position:absolute; right:0; width:120px; height:80%; top:0; display:flex; justify-content: space-around; flex-direction: column;">
-<span>온도</span>
-<?php echo $row['temperature']; ?>°C
+<div style="position:absolute; right:0; width:130px; height:60%; top:30px; display:flex; justify-content: space-around; flex-direction: column;">
+<span style="font-size:20px;">온도</span>
+<?php echo '<span style="font-size:25px; font-weight:bold;">' . $row['temperature'] . " °C</span>"; ?>
 </div>
-    <div id="cap" style="position:absolute;top:55px;left:47px;text-align:center;font-size:20px;font-family:Arial, sans-serif;">
+    <div id="cap" style="position:absolute;top:73px;left:62px;text-align:center;font-size:20px;font-family:Arial, sans-serif;">
 	<?php echo $row['temperature']; ?>°C
 	</div>
 </div>
 <div style="position:relative;width:280px; height:170px; box-shadow: 0px 11px 30px rgba(0, 0, 0, 0.25); border-radius:10px;">
-<div style="width:120px; margin:8px;">
+<div style="width:120px; margin-left:25px; margin-top:25px;">
     <canvas id="humidityChart"></canvas>
 </div>
-<div style="position:absolute; right:0; width:120px; height:80%; top:0; display:flex; justify-content: space-around; flex-direction: column;">
-<span>습도</span>
-<?php echo $row['humidity']; ?>%
+<div style="position:absolute; right:0; width:130px; height:60%; top:30px; display:flex; justify-content: space-around; flex-direction: column;">
+<span style="font-size:20px;">습도</span>
+<?php echo '<span style="font-size:25px; font-weight:bold;">' . $row['humidity'] . " %</span>"; ?>
 </div>
-    <div id="cap" style="position:absolute;top:55px;left:47px;text-align:center;font-size:20px;font-family:Arial, sans-serif;">
+    <div id="cap" style="position:absolute;top:73px;left:62px;text-align:center;font-size:20px;font-family:Arial, sans-serif;">
 	<?php echo $row['humidity']; ?>%
 	</div>
 </div>
@@ -48,7 +48,7 @@ $row = mysqli_fetch_assoc($result);
         var temperatureChart = new Chart(temperatureCtx, {
             type: 'doughnut',
             data: {
-                labels: ['Temperature (°C)', ''],
+                labels: ['온도 (°C)', ''],
                 datasets: [
                     {
                         data: [temperature, 100 - temperature],
@@ -71,7 +71,7 @@ $row = mysqli_fetch_assoc($result);
         var humidityChart = new Chart(humidityCtx, {
             type: 'doughnut',
             data: {
-                labels: ['Humidity (%)', ''],
+                labels: ['습도 (%)', ''],
                 datasets: [
                     {
                         data: [humidity, 100 - humidity],
