aa689d48b4301a3650a3c49ad4408bae141c96ad
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-05-18 10:28:47 +0900

    Fix :: 시간 비교포맷 변경

--- fetch.php
+++ fetch.php
@@ -2,8 +2,18 @@
 include 'db.php';
 $conn = mysqli_connect('localhost', $db_id, $db_pw, $db_name);
 
-$query = "SELECT * FROM tbl ORDER BY id ASC LIMIT 7;";
+$query = "SELECT * FROM tbl ORDER BY id DESC LIMIT 7;";
 $result = mysqli_query($conn, $query);
+
+$query = "SELECT * FROM time LIMIT 1;";
+$result_time = mysqli_query($conn, $query);
+if ($row_time = mysqli_fetch_assoc($result_time)) {
+    $startTime = $row_time['start'];
+    $endTime = $row_time['end'];
+} else {
+    $startTime = '07:30';
+    $endTime = '20:30';
+}
 
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr>";
@@ -11,10 +21,9 @@ while ($row = mysqli_fetch_assoc($result)) {
     echo "<td>".$row['rt']."</td>";
     echo "<td>".($row['motion'] == 1 ? 'O' : 'X')."</td>";
     $rtTime = strtotime($row['rt']);
-    $startTime = strtotime('07:30');
-    $endTime = strtotime('20:30');
-    $inWorkHours = $row['motion'] == 1 && $rtTime >= $startTime && $rtTime <= $endTime;
-    echo "<td>".($inWorkHours ? 'O' : 'X')."</td>";
+    $rtFormatted = date("H:i", $rtTime);
+    $inWorkHours = $row['motion'] == 1 && $rtFormatted >= $startTime && $rtTime <= $endTime;
+    echo "<td>" . ($inWorkHours ? 'O' : 'X') . "</td>";
     echo "<td>";
     echo '<a style="text-decoration: none;" href="?delete_id='.$row['id'].'" onclick="return confirm(\'정말로 삭제 하시겠습니까?\');" class="delete">삭제</a>';
     echo "</td>";
