0b793153c0868072de1902ec6121b798aeea1140
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-05-28 23:27:29 +0900

    Add :: 로그인시 접근가능

--- manage.php
+++ manage.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (!isset($_SESSION["username"])) {
+  echo '<script>alert("로그인이 필요합니다.");</script>';
+  echo '<script>window.location.href = "login.php";</script>';
+  exit();
+}
+?>
+
 <!DOCTYPE html>
 <link rel="stylesheet" href="css/main.css">
 <html lang="ko">
