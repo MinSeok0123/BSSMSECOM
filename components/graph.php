b37d75eea2044df86a0cf58307b6129999e7c45d
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-06-02 09:00:50 +0900

    Add :: graph ajax

--- components/graph.php
+++ components/graph.php
@@ -3,73 +3,92 @@ session_start();
 include 'db.php';
 $conn = mysqli_connect('localhost', $db_id, $db_pw, $db_name);
 $id = $_SESSION["id"];
-$query = "SELECT * FROM tbl WHERE account = '$id' ORDER BY rt ASC LIMIT 7;";
+$query = "SELECT * FROM tbl WHERE account = '$id' ORDER BY rt DESC LIMIT 7;";
 $result = mysqli_query($conn, $query);
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>온습도 그래프</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <canvas id="myChart"></canvas>
     <script>
-    var temperatureData = [];
-    var humidityData = [];
-    var timeData = [];
+    function refreshChart() {
+        var temperatureData = [];
+        var humidityData = [];
+        var timeData = [];
 
-    <?php
-    while ($row = mysqli_fetch_assoc($result)) {
-    $formattedTime = date("g시 i분 s초", strtotime($row['rt']));
-    echo "temperatureData.push(" . $row['temperature'] . ");";
-    echo "humidityData.push(" . $row['humidity'] . ");";
-    echo "timeData.push('" . $formattedTime . "');";
-}
-    ?>
+        <?php
+        while ($row = mysqli_fetch_assoc($result)) {
+            $formattedTime = date("g시 i분 s초", strtotime($row['rt']));
+            echo "temperatureData.push(" . $row['temperature'] . ");";
+            echo "humidityData.push(" . $row['humidity'] . ");";
+            echo "timeData.push('" . $formattedTime . "');";
+        }
+        ?>
 
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: timeData,
-            datasets: [
-                {
-                    label: '온도 (°C)',
-                    data: temperatureData,
-                    borderColor: '#E74040',
-                    fill: false
-                },
-                {
-                    label: '습도 (%)',
-                    data: humidityData,
-                    borderColor: '#40C0E7',
-                    fill: false
-                }
-            ]
-        },
-        options: {
-            responsive: true,
-            scales: {
-                x: {
-                    display: true,
-                    title: {
-                        display: true,
-                        text: '시간'
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: timeData,
+                datasets: [
+                    {
+                        label: '온도 (°C)',
+                        data: temperatureData,
+                        borderColor: '#E74040',
+                        fill: false
+                    },
+                    {
+                        label: '습도 (%)',
+                        data: humidityData,
+                        borderColor: '#40C0E7',
+                        fill: false
                     }
-                },
-                y: {
-                    display: true,
-                    title: {
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    x: {
                         display: true,
-                        text: '값'
+                        title: {
+                            display: true,
+                            text: '시간'
+                        }
+                    },
+                    y: {
+                        display: true,
+                        title: {
+                            display: true,
+                            text: '값'
+                        }
                     }
                 }
             }
-        }
-    });
+        });
+        
+        setInterval(function() {
+            $.ajax({
+                url: 'graph_update.php',
+                success: function(data) {
+                    var newData = JSON.parse(data);
+                    
+                    chart.data.datasets[0].data = newData.temperatureData;
+                    chart.data.datasets[1].data = newData.humidityData;
+                    chart.data.labels = newData.timeData;
+                    
+                    chart.update();
+                }
+            });
+        }, 1000);
+    }
+
+    refreshChart();
     </script>
 </body>
 </html>
---
+++ update_graph.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'db.php';
+$conn = mysqli_connect('localhost', $db_id, $db_pw, $db_name);
+$id = $_SESSION["id"];
+$query = "SELECT * FROM tbl WHERE account = '$id' ORDER BY rt DESC LIMIT 7;";
+$result = mysqli_query($conn, $query);
+
+$temperatureData = [];
+$humidityData = [];
+$timeData = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $formattedTime = date("g시 i분 s초", strtotime($row['rt']));
+    $temperatureData[] = $row['temperature'];
+    $humidityData[] = $row['humidity'];
+    $timeData[] = $formattedTime;
+}
+
+$data = [
+    'temperatureData' => $temperatureData,
+    'humidityData' => $humidityData,
+    'timeData' => $timeData
+];
+
+echo json_encode($data);
+?>
